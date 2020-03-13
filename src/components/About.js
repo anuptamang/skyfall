3d25fef49f5303f646bd6ce260f41848b49295a3
Anup Tamang <anupkumartamang@gmail.com>
2020-03-13 09:20:11 +0545

    changes in dev-mode branch

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
       <div className="container py-10 py-lg-22">
         <div className="row mx-lg-n10">
           <div className="col-md-6 px-lg-10">
-            <h2>WHO WE ARE & <br></br>WHAT WE DO</h2>
+            <h2>WHAT WE DO</h2>
             <p>The Skyfall Technologies provides various IT-related services to private and government organizations. We also offer support services like IT Trainings and an industry-leading Support Team ready to help clients develop their system, softwares, apps and websites. </p>
           </div>
           <div className="col-md-6 px-lg-10">
