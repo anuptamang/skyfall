0ec87ee7fdb5394023342e055da73670bb5cdd1c
Anup Tamang <anupkumartamang@gmail.com>
2021-01-06 08:26:48 +0545

    team updated

--- src/components/Team.js
+++ src/components/Team.js
@@ -4,14 +4,12 @@ import imgAnil from '../images/team/anil.jpg';
 import imgPramod from '../images/team/pramod.jpg';
 import imgDharmendra from '../images/team/dharmendra.jpg';
 import imgManoj from '../images/team/manoj.jpg';
-import imgApar from '../images/team/apar.jpg';
-//import imgSubodh from '../images/team/subodh.jpg';
+import imgSubodh from '../images/team/subodh.jpg';
 import imgShankar from '../images/team/shankar.jpg';
-//import imgSamrat from '../images/team/samrat.jpg';
-import imgMotilal from '../images/team/motilal.jpg';
-import imgAnju from '../images/team/anju.jpg';
-import imgNirmala from '../images/team/nirmala.jpg';
+import imgSamrat from '../images/team/samrat.jpg';
 import imgSantosh from '../images/team/santosh.jpg';
+import imgSabina from '../images/team/sabina.JPG';
+import imgSapana from '../images/team/sapana.JPG';
 
 export default function About() {
   return (
@@ -20,58 +18,91 @@ export default function About() {
         <h2 className="mb-15">Meet Our Team</h2>
         <div className="row justify-content-center">
           <div className="col-6 col-md-3 mb-8 mb-lg-12">
-            <div className="img-holder mx-auto mb-3" style={{ backgroundImage: `url(${imgAnup})` }} ></div>
+            <div
+              className="img-holder mx-auto mb-3"
+              style={{ backgroundImage: `url(${imgAnup})` }}
+            ></div>
             <h4 className="mb-1">Anup K. Tamang</h4>
             <p>Founder</p>
           </div>
           <div className="col-6 col-md-3 mb-8 mb-lg-12">
-            <div className="img-holder mx-auto mb-3" style={{ backgroundImage: `url(${imgMotilal})` }} ></div>
-            <h4 className="mb-1">Moti Lal Rai</h4>
-            <p>Chief Executive Officer</p>
+            <div
+              className="img-holder mx-auto mb-3"
+              style={{ backgroundImage: `url(${imgPramod})` }}
+            ></div>
+            <h4 className="mb-1">Pramod Bhandari</h4>
+            <p>Managing Director</p>
           </div>
           <div className="col-6 col-md-3 mb-8 mb-lg-12">
-            <div className="img-holder mx-auto mb-3" style={{ backgroundImage: `url(${imgAnil})` }} ></div>
+            <div
+              className="img-holder mx-auto mb-3"
+              style={{ backgroundImage: `url(${imgAnil})` }}
+            ></div>
             <h4 className="mb-1">Anil K. Tamang</h4>
             <p>Chief Technology Officer</p>
           </div>
           <div className="col-6 col-md-3 mb-8 mb-lg-12">
-            <div className="img-holder mx-auto mb-3" style={{ backgroundImage: `url(${imgManoj})` }} ></div>
+            <div
+              className="img-holder mx-auto mb-3"
+              style={{ backgroundImage: `url(${imgManoj})` }}
+            ></div>
             <h4 className="mb-1">Manoj Ban</h4>
             <p>Business Development</p>
           </div>
           <div className="col-6 col-md-3 mb-8 mb-lg-12">
-            <div className="img-holder mx-auto mb-3" style={{ backgroundImage: `url(${imgDharmendra})` }} ></div>
+            <div
+              className="img-holder mx-auto mb-3"
+              style={{ backgroundImage: `url(${imgDharmendra})` }}
+            ></div>
             <h4 className="mb-1">Dharmendra Rai</h4>
             <p>Project Manager</p>
           </div>
           <div className="col-6 col-md-3 mb-8 mb-lg-12">
-            <div className="img-holder mx-auto mb-3" style={{ backgroundImage: `url(${imgPramod})` }} ></div>
-            <h4 className="mb-1">Pramod Bhandari</h4>
-            <p>Software Developer</p>
-          </div>
-          <div className="col-6 col-md-3 mb-8 mb-lg-12">
-            <div className="img-holder mx-auto mb-3" style={{ backgroundImage: `url(${imgShankar})` }} ></div>
+            <div
+              className="img-holder mx-auto mb-3"
+              style={{ backgroundImage: `url(${imgShankar})` }}
+            ></div>
             <h4 className="mb-1">Shankar Bhattarai</h4>
-            <p>Software Developer</p>
+            <p>Software Engineer</p>
           </div>
           <div className="col-6 col-md-3 mb-8 mb-lg-12">
-            <div className="img-holder mx-auto mb-3" style={{ backgroundImage: `url(${imgApar})` }} ></div>
-            <h4 className="mb-1">Apar Magar</h4>
+            <div
+              className="img-holder mx-auto mb-3"
+              style={{ backgroundImage: `url(${imgSubodh})` }}
+            ></div>
+            <h4 className="mb-1">Subodh Upreti</h4>
+            <p>Software Engineer</p>
+          </div>
+          <div className="col-6 col-md-3 mb-8 mb-lg-12">
+            <div
+              className="img-holder mx-auto mb-3"
+              style={{ backgroundImage: `url(${imgSamrat})` }}
+            ></div>
+            <h4 className="mb-1">Samrat KC</h4>
             <p>Graphics Designer</p>
           </div>
           <div className="col-6 col-md-3 mb-8 mb-lg-12">
-            <div className="img-holder mx-auto mb-3" style={{ backgroundImage: `url(${imgSantosh})` }} ></div>
+            <div
+              className="img-holder mx-auto mb-3"
+              style={{ backgroundImage: `url(${imgSantosh})` }}
+            ></div>
             <h4 className="mb-1">Santosh Rai</h4>
             <p>Network & Support</p>
           </div>
           <div className="col-6 col-md-3 mb-8 mb-lg-12">
-            <div className="img-holder mx-auto mb-3" style={{ backgroundImage: `url(${imgAnju})` }} ></div>
-            <h4 className="mb-1">Anju Rai</h4>
+            <div
+              className="img-holder mx-auto mb-3"
+              style={{ backgroundImage: `url(${imgSabina})` }}
+            ></div>
+            <h4 className="mb-1">Sabina Shrestha</h4>
             <p>Sales & Marketing</p>
           </div>
           <div className="col-6 col-md-3 mb-8 mb-lg-12">
-            <div className="img-holder mx-auto mb-3" style={{ backgroundImage: `url(${imgNirmala})` }} ></div>
-            <h4 className="mb-1">Nirmala Chapagain</h4>
+            <div
+              className="img-holder mx-auto mb-3"
+              style={{ backgroundImage: `url(${imgSapana})` }}
+            ></div>
+            <h4 className="mb-1">Sapana Rai</h4>
             <p>Sales & Marketing</p>
           </div>
         </div>
