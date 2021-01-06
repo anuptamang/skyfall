5765ac3cc5d048a589196a429d656eece9763fe4
Anup Tamang <anuptamang@anuptamang-mbp.local>
2021-01-06 08:56:47 +0545

    Updates

---
+++ precache-manifest.21a394e4a3b0d98c0f96f90370fc2871.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9abb787d9abf7af4a2aec6c1ecd9beb",
+    "url": "/skyfall/index.html"
+  },
+  {
+    "revision": "53a6203b356663b0e319",
+    "url": "/skyfall/static/css/main.64fd7879.chunk.css"
+  },
+  {
+    "revision": "d9b662e3bde36b7ed858",
+    "url": "/skyfall/static/js/2.b10f746d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/skyfall/static/js/2.b10f746d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a6203b356663b0e319",
+    "url": "/skyfall/static/js/main.e7e26844.chunk.js"
+  },
+  {
+    "revision": "e75fc924a996b77e0cfe",
+    "url": "/skyfall/static/js/runtime-main.e884f686.js"
+  },
+  {
+    "revision": "b43826a54b835b5256c77b24926e42c9",
+    "url": "/skyfall/static/media/about-post.b43826a5.jpg"
+  },
+  {
+    "revision": "17d5c6eae116416f89cfcd83dbf69599",
+    "url": "/skyfall/static/media/anil.17d5c6ea.jpg"
+  },
+  {
+    "revision": "214901fb02a1be62cf17ec44e48ab78c",
+    "url": "/skyfall/static/media/anup.214901fb.jpg"
+  },
+  {
+    "revision": "aef620882b40a07f2f5cd3a9ecaf13db",
+    "url": "/skyfall/static/media/bipin.aef62088.jpg"
+  },
+  {
+    "revision": "b655a99b4f045951687a9d80bca47ff3",
+    "url": "/skyfall/static/media/dharmendra.b655a99b.jpg"
+  },
+  {
+    "revision": "c31de212c571c433362faaaca3fae15d",
+    "url": "/skyfall/static/media/hero-banner-2.c31de212.jpg"
+  },
+  {
+    "revision": "5deda86a3bae4668b1b92862bd7e5437",
+    "url": "/skyfall/static/media/manoj.5deda86a.jpg"
+  },
+  {
+    "revision": "890b0d6dee5fe5517c761308e01189dc",
+    "url": "/skyfall/static/media/pramod.890b0d6d.jpg"
+  },
+  {
+    "revision": "37ff1e67926830fdb6063f22f17f96f4",
+    "url": "/skyfall/static/media/sabina.37ff1e67.JPG"
+  },
+  {
+    "revision": "196c56ca8eeafb5bd1043f35f5ca5924",
+    "url": "/skyfall/static/media/santosh.196c56ca.jpg"
+  },
+  {
+    "revision": "6cf8979875d56e380ec42fa939e554ac",
+    "url": "/skyfall/static/media/sapana.6cf89798.JPG"
+  },
+  {
+    "revision": "e3068fcfeabc076e0dec03970a90bda1",
+    "url": "/skyfall/static/media/services-banner.e3068fcf.jpg"
+  },
+  {
+    "revision": "413d5fdb44693d8711bb4194ca3f4ddf",
+    "url": "/skyfall/static/media/shankar.413d5fdb.jpg"
+  },
+  {
+    "revision": "e0f69cde0d3f2e691198d9c3aa33c7dc",
+    "url": "/skyfall/static/media/skyfall.e0f69cde.svg"
+  },
+  {
+    "revision": "9cf64f4f5d2b7216707997b42541ae46",
+    "url": "/skyfall/static/media/subodh.9cf64f4f.jpg"
+  }
+]);
